0b83529228ebb45ca1d33cddaf29017df92456f0
Issa Rice <riceissa@gmail.com>
2017-10-06 12:18:18 -0700

    Initial version

---
+++ proc_csv.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+import csv
+
+
+def mysql_quote(x):
+    '''
+    Quote the string x using MySQL quoting rules. If x is the empty string,
+    return "NULL". Probably not safe against maliciously formed strings, but
+    whatever; our input is fixed and from a basically trustable source..
+    '''
+    if not x:
+        return "NULL"
+    x = x.replace("\\", "\\\\")
+    x = x.replace("'", "''")
+    x = x.replace("\n", "\\n")
+    return "'{}'".format(x)
+
+
+def mysql_number(x):
+    if not x:
+        return "NULL"
+    x = x.strip()
+    x = x.replace(",", "")
+    return x
+
+print("""insert into maddison(region, year, database_url,
+         data_retrieval_method, metric, units, value, notes) values""")
+
+
+with open("mpd_2013-01.csv", newline='') as f:
+    reader = csv.DictReader(f)
+    first = True
+
+    for row in reader:
+        for region in row:
+            print(region)
+            r = region.strip()
+            if r != "Year":
+                print(r, row["Year"], row[region])
+                print("    " + ("" if first else ",") + "(" + ",".join([
+                    mysql_quote(r),  # region
+                    mysql_number(row["Year"]),  # year
+                    mysql_quote("http://www.ggdc.net/maddison/maddison-project/data/mpd_2013-01.xlsx"),  # database_url
+                    mysql_quote(""),  # data_retrieval_method
+                    mysql_quote("GDP per capita"),  # metric
+                    mysql_quote("1990 international Geary–Khamis dollars"),  # units
+                    mysql_number(row[region]),  # value
+                    mysql_quote(""),  # notes
+                ]) + ")")
+                first = False
+    print(";")
