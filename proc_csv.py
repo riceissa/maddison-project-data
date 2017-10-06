d6bf8415f7393b5deaedb6020199cb2275e508c4
Issa Rice <riceissa@gmail.com>
2017-10-06 13:30:58 -0700

    separate out utility functions

--- proc_csv.py
+++ proc_csv.py
@@ -2,27 +2,8 @@
 
 import csv
 
+from util import *
 
-def mysql_quote(x):
-    '''
-    Quote the string x using MySQL quoting rules. If x is the empty string,
-    return "NULL". Probably not safe against maliciously formed strings, but
-    whatever; our input is fixed and from a basically trustable source..
-    '''
-    if not x:
-        return "NULL"
-    x = x.replace("\\", "\\\\")
-    x = x.replace("'", "''")
-    x = x.replace("\n", "\\n")
-    return "'{}'".format(x)
-
-
-def mysql_number(x):
-    if not x:
-        return "NULL"
-    x = x.strip()
-    x = x.replace(",", "")
-    return x
 
 print("""insert into maddison(region, year, database_url,
          data_retrieval_method, metric, units, value, notes) values""")
