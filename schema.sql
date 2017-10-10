6c25c61d80bfa90ae610bf6d7ea4c65ef73d029f
Issa Rice <riceissa@gmail.com>
2017-10-10 12:54:13 -0700

    generalize table name

--- schema.sql
+++ schema.sql
@@ -1,9 +1,9 @@
-create table maddison (
+create table data (
     estimate_id int(11) not null auto_increment primary key,
     region varchar(100),
     year int(10),
     database_url varchar(200),
-    data_retrieval_method varchar(100),
+    data_retrieval_method varchar(1000),
     metric varchar(100),
     units varchar(100),
     value float(14,2),
