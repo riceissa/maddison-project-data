d3df611b881ce865fdfe8307b839310d852b0782
Issa Rice <riceissa@gmail.com>
2017-10-12 17:52:01 -0700

    increase metric length

--- schema.sql
+++ schema.sql
@@ -4,7 +4,7 @@ create table data (
     year int(10),
     database_url varchar(200),
     data_retrieval_method varchar(1000),
-    metric varchar(100),
+    metric varchar(200),
     units varchar(100),
     value float(28,14),
     notes varchar(2000) DEFAULT NULL
