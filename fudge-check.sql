d9d4d3b525bc29233f475381c9bf7a4a52646cd1
Vipul Naik <vipulnaik1@gmail.com>
2017-10-13 09:33:06 -0700

    Add fudge check

--- fudge-check.sql
+++ fudge-check.sql
@@ -1,4 +1,4 @@
-create temporary table dt1 as select coalesce(sum(case when metric = 'GDP' then value else 0 end), 0) as gdp, coalesce(sum(case when metric = 'GDP per capita' then value else 0 end), 0) as gdppc, coalesce(sum(case when metric = 'Population' then value else 0 end), 0) as pop from data where database_url = 'http://www.ggdc.net/maddison/Historical_Statistics/horizontal-file_02-2010.xls' group by region, year;
+create temporary table dt1 as select region, year, coalesce(sum(case when metric = 'GDP' then value else 0 end), 0) as gdp, coalesce(sum(case when metric = 'GDP per capita' then value else 0 end), 0) as gdppc, coalesce(sum(case when metric = 'Population' then value else 0 end), 0) as pop from data where database_url = 'http://www.ggdc.net/maddison/Historical_Statistics/horizontal-file_02-2010.xls' group by region, year;
 
 create temporary table dt2 as select *, gdp / (1000 * pop * gdppc) as  fudge from dt1 where pop > 0 and gdppc > 0;
 
