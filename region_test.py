02e090983a9f4f452593b6472384d95a2b3330ca
Issa Rice <riceissa@gmail.com>
2017-11-22 20:01:09 -0800

    use new import

--- region_test.py
+++ region_test.py
@@ -2,10 +2,10 @@
 
 import sys
 
-import util
+import devec_sql_common
 
 
 if __name__ == "__main__":
     for line in sys.stdin:
         region = line.strip()
-        print(region + "\t" + util.REGION_MAP.get(region.lower(), ""))
+        print(region + "\t" + devec_sql_common.REGION_MAP.get(region.lower(), ""))
